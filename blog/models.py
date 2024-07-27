30505c271dc90dab39d5ff56c60b57626b6614a1
Bhavanishankar2004 <komatibhavanishankar@gmail.com>
2024-07-28 03:03:26 +0530

    comment model

--- blog/models.py
+++ blog/models.py
@@ -24,3 +24,11 @@ class Post(models.Model):
     
 
     
+class comment(models.Model):
+    post =  models.ForeignKey(Post,related_name="comments" ,on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    body= models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.post.title,self.name)
